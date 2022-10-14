96571311aba3d7c9a19b297c78d2e574c8832210
LiquidIce95 <114932597+LiquidIce95@users.noreply.github.com>
2022-10-14 19:19:38 +0200

    Add files via upload

---
+++ TestSuite.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include <vector>
+#include <sstream>
+#include <iostream>
+
+class TestCase {
+    //converts types into string , so we can append to messages, << operator needs to be defined for tpye T
+    template<class T> std::string tostring(const T& t) {
+	    std::ostringstream ss;
+	    ss << t;
+	    return ss.str();
+    }
+
+    public:
+        std::vector<bool> tests;
+        std::vector<std::string> msgs;
+
+        void asser(std::string testname , auto correct, auto output){
+
+            const std::string red("\033[1;31m");
+            const std::string green("\033[1;32m");
+            std::string reset("\033[0m");
+
+
+            try {
+                if(correct == output){
+                    std::cout<<green<<"."<<reset;
+                    this->tests.push_back(true);
+                } else {
+                    std::cout<<red<<"."<<reset;
+                    this->tests.push_back(false);
+                    std::string message = testname+" FAILED ,expected: "+tostring(correct)+" actual: "+tostring(output);
+                    this->msgs.push_back(message);
+                }
+
+            }
+            catch(const std::exception& e){
+                std::cout<<red<<"."<<reset;
+                this->tests.push_back(false);
+                this->msgs.push_back(testname+" ERROR occured"+e.what());
+            }
+            
+        }
+
+        void feedback(){
+            std::cout<<std::endl;
+            for (int i = 0; i < msgs.size(); ++i){
+                std::cout<<msgs.at(i)<<std::endl;
+            }
+
+            if(msgs.size()==0){
+                std::cout<<"All tests were successful!"<<std::endl;
+            }
+        }
+
+};
